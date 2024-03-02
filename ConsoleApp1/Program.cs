4932dc79f1ac6757ae4a3f6e8a66752c372fb218
JahSupansa <157448538+JahSupansa@users.noreply.github.com>
2024-03-02 20:52:52 +0700

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true) 
+            {
+            Console.Write("Enter Your Namber: ");
+            int number = int.Parse(Console.ReadLine());
+
+            string output = "";
+
+            if (number % 3 == 0)
+            {
+                output += "Hello";
+            }
+            if (number % 5 == 0)
+            {
+                output += "World";
+            }
+            if (number % 7 == 0)
+            {
+                output += "My";
+            }
+
+            if (output == "")
+            {
+                Console.WriteLine(number);
+            }
+            else
+            {
+                Console.WriteLine(output);
+            }
+
+            }
+        }
+    }
+}
